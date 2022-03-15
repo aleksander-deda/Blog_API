562b64674dd38960b0824aec7115b554527b4535
Aleksi <aleksi1.deda@gmail.com>
2022-03-15 15:34:54 +0100

    adding some changes

--- blog/hashing.py
+++ blog/hashing.py
@@ -5,3 +5,7 @@ pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")     # making the p
 class Hash():
     def bcrypt(password: str):
         return pwd_cxt.hash(password)
+
+
+    def verify(hashed_password, plain_password):
+        return pwd_cxt.verify(plain_password ,hashed_password)
